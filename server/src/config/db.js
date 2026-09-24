69a5794b94199796de9aa5498bbdbd6c125b5978
sanchit <10dsanchitbis.virdi@gmail.com>
2026-09-25 00:09:31 +0530

    feat: configure mysql connection pooling

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,13 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+
+    connectionLimit: 10,
+
+    waitForConnections: true,
+
+    queueLimit: 0
 });
 
 module.exports = pool;
